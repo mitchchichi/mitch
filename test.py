d3e08a93b8165fccf23e05062843b8d656fcc12b
mitchchichi <mitch.chichi@gmail.com>
2016-12-17 14:32:14 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg950\cocoartf1404\cocoasubrtf110
+{\fonttbl\f0\fnil\fcharset136 PingFangTC-Regular;}
+{\colortbl;\red255\green255\blue255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww33400\viewh18180\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 i=93\
+print(i)\
+\
+}
